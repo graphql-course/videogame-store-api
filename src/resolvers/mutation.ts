e6637a5cf0ddb384ac05e71bfc1c6b531cab25a5
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-21 15:05:00 +0100

    API with tslint and clean

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,36 +1,36 @@
-import { IGame } from './../interfaces/game.interface';
+import { IGame } from "./../interfaces/game.interface";
 import { IResolvers } from "graphql-tools";
-import { gamesItems } from '../data/data';
+import { gamesItems } from "../data/data";
 const mutation: IResolvers = {
-    Mutation: {
-        createGame: (_: void, { title, platformsIds }: IGame) => {
-    
-          const id = `${gamesItems.length + 1}`;
-    
-          gamesItems.push({
-            id,
-            title,
-            platformsIds,
-          });
-        },
-    
-        deleteGame: (parent: any, args: IGame) => {
-          const { id } = args;
-    
-          /*const book = books.find(e => e.id === id);
-    
-          let delPos = 0;
-          const elementDelete = books.map( (book, index) => {
-              delPos = 1;
-          });*/
+  Mutation: {
+    createGame: (_: void, { title, platformsIds }: IGame) => {
+      const id = `${gamesItems.length + 1}`;
+      const newGame = {
+        id,
+        title,
+        platformsIds
+      };
+      gamesItems.push(newGame);
+      return newGame;
+    },
 
-          // Delete select book
+    deleteGame: (parent: any, args: IGame) => {
+      const { id } = args;
 
-          // books.splice(delPos, 1);
-    
-          return undefined;
-        },
-      },
-}
+      /*const book = books.find(e => e.id === id);
+  
+        let delPos = 0;
+        const elementDelete = books.map( (book, index) => {
+            delPos = 1;
+        });*/
+
+      // Delete select book
+
+      // books.splice(delPos, 1);
+
+      return undefined;
+    }
+  }
+};
 
 export default mutation;
